955c2b30322d02061b6bf1557fe6a0cd69e7d302
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-10 16:11:33 -0300

    App layout

---
+++ src/layout/DefaultLayout/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  .container {
+    width: 100%;
+    max-width: 56rem;
+
+    padding-left: 1rem;
+    padding-right: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`
