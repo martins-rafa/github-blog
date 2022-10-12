ece4eeb7d7a0bef501fc3b6e9736225bb0a5121e
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-12 14:40:01 -0300

    implement search functionality

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,15 +1,37 @@
 import { SearchFormContainer } from './styles'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export function SearchForm() {
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInput = z.infer<typeof searchFormSchema>
+
+interface SearchFormProps {
+  postsListLength: number
+  fetchIssues: (query?: string) => Promise<void>
+}
+
+export function SearchForm({ postsListLength, fetchIssues }: SearchFormProps) {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearch(data: SearchFormInput) {
+    await fetchIssues(data.query)
+  }
+
   return (
     <SearchFormContainer>
       <div>
         <h2>Posts</h2>
-        <span>6 posts</span>
+        <span>{postsListLength} posts</span>
       </div>
 
-      <form action="">
-        <input type="text" placeholder="Search" />
+      <form onSubmit={handleSubmit(handleSearch)}>
+        <input type="text" placeholder="Search" {...register('query')} />
       </form>
     </SearchFormContainer>
   )
