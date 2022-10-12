81c01b0ffef2e6322b2786b219f14b0eb982f2d9
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-12 14:00:07 -0300

    use react-mardown lib to style and display the issue body

--- src/pages/Post/components/PostMainContent/index.tsx
+++ src/pages/Post/components/PostMainContent/index.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/no-children-prop */
 import { PostMainContentContainer } from './styles'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface PostMainContentProps {
   content: string
@@ -7,7 +11,27 @@ interface PostMainContentProps {
 export function PostMainContent({ content }: PostMainContentProps) {
   return (
     <PostMainContentContainer>
-      <p>{content}</p>
+      <ReactMarkdown
+        children={content}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={dark as any}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      />
     </PostMainContentContainer>
   )
 }
--- src/pages/Post/components/PostMainContent/styles.ts
+++ src/pages/Post/components/PostMainContent/styles.ts
@@ -1,5 +1,47 @@
 import styled from 'styled-components'
 
 export const PostMainContentContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   padding: 2.5rem 2rem;
+  margin-bottom: 8rem;
+
+  img {
+    width: 100%;
+  }
+
+  h1,
+  h2,
+  h3 {
+    color: ${({ theme }) => theme.colors.blue};
+  }
+
+  ul {
+    list-style: inherit;
+    padding-left: 1.5rem;
+  }
+
+  a {
+    text-decoration: underline;
+    color: ${({ theme }) => theme.colors.blue};
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  pre {
+    background: ${({ theme }) => theme.colors['base-post']};
+    padding: 1rem;
+
+    > div {
+      background: none !important;
+      padding: 0 !important;
+      margin: 0 !important;
+
+      code {
+        font-family: 'Fira Code', monospace !important;
+        line-height: 160% !important;
+      }
+    }
+  }
 `
